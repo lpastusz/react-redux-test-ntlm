7e119bad3c9ebf3c17133f8b595a3b9712feadad
Lukas Pastuszek <lukas.pastuszek@gmail.com>
2018-03-23 23:59:11 +0100

    Edit src

--- src/index.js
+++ src/index.js
@@ -5,8 +5,21 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import helloReducer from './reducers'
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-let store = createStore(helloReducer) // this is store
+let store = createStore(helloReducer);
+
+axios({
+  method:'get',
+  url:'https://www.reddit.com/subreddits/search.json?q=ntlm',
+  withCredentials: true,
+})
+.then(function (response) {
+  console.log(response);
+})
+.catch(function (error) {
+  console.log(error);
+});
 
 ReactDOM.render(
   <Provider store={store}>
